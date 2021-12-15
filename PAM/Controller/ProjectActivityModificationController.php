26de87ca81bd81e88d1e3d47e31662b89d996ba5
Tomas Dittmann <chaosmeist3r@gmail.com>
2021-12-15 22:37:03 +0100

    add missing delete function

--- PAM/Controller/ProjectActivityModificationController.php
+++ PAM/Controller/ProjectActivityModificationController.php
@@ -17,6 +17,16 @@ class ProjectActivityModificationController extends BaseController
             )
         ));
     }
+    
+    public function delete()
+    {
+        if ($this->userSession->isAdmin()) {
+            $event_id = $this->request->getIntegerParam('event_id');
+            $this->projectActivityModificationModel->delete($event_id);
+        }
+
+        $this->response->redirect('/');
+    }
 
     public function toggleVisibility()
     {
