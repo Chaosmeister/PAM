6cfdbead6d7033773ea8e21b492fdb416645c2c7
Tomas Dittmann <chaosmeist3r@gmail.com>
2021-12-15 23:00:42 +0100

    Delete PAM directory

--- PAM/Assets/js/pam.js
+++
@@ -1,7 +0,0 @@
-KB.on('dom.ready', function () {
-    KB.onClick(".PAM_toggleVisibility", function (e) {
-      KB.http.get("/?controller=ProjectActivityModificationController&action=toggleVisibility&plugin=PAM&event_id=" + e.target.dataset.event_id);
-      e.target.children[0].classList.toggle('fa-eye-slash');
-      e.target.children[0].classList.toggle('fa-eye');
-  });
-});
